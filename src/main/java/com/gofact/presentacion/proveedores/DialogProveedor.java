4d6213034ae05857c446b401469b7e5377d66cdc
Nath05 <nathy.cumbicos@gmail.com>
2015-11-21 10:09:05 -0500

    Commit Vistas Proveedor

---
+++ src/main/java/com/gofact/presentacion/proveedores/DialogProveedor.java
@@ -0,0 +1,325 @@
+package com.gofact.presentacion.proveedores;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class DialogProveedor extends javax.swing.JDialog {
+    
+    
+    public DialogProveedor(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        setLocationRelativeTo(null);
+        
+    }
+    
+    private void habilitarModificacion(boolean estado){
+         cmdNuevo.setEnabled(!estado);
+         cmdGuardar.setEnabled(estado);
+         cmdCancelar.setEnabled(estado);
+         cmdEditar.setEnabled(!estado);
+         cmdEliminar.setEnabled(!estado);
+         gridProveedores.setVisible(!estado);
+         txtRUC.setEnabled(estado);
+         txtRazonSocial.setEnabled(estado);
+         txtDireccion.setEnabled(estado);
+         txtTelefono.setEnabled(estado);
+         txtNombreComercial.setEnabled(estado);
+    }
+    public void setIdUsuario(int idUsuario){
+        //this.idUsuario = idUsuario;
+    }
+    
+    private void ocultarColumnas(){
+    }
+      
+    private void limpiar() {
+        
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        cmdNuevo = new javax.swing.JButton();
+        cmdEditar = new javax.swing.JButton();
+        cmdEliminar = new javax.swing.JButton();
+        cmdGuardar = new javax.swing.JButton();
+        cmdCancelar = new javax.swing.JButton();
+        lblTotalRegs = new javax.swing.JLabel();
+        txtRUC = new javax.swing.JTextField();
+        txtRazonSocial = new javax.swing.JTextField();
+        txtTelefono = new javax.swing.JTextField();
+        txtDireccion = new javax.swing.JTextField();
+        lblRuc = new javax.swing.JLabel();
+        lblRazonSocial = new javax.swing.JLabel();
+        lblDireccion = new javax.swing.JLabel();
+        lblTelefono = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        gridProveedores = new javax.swing.JTable();
+        lblNombreComercial = new javax.swing.JLabel();
+        txtNombreComercial = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setResizable(false);
+
+        cmdNuevo.setText("Nuevo");
+        cmdNuevo.setName("cmdNuevo"); // NOI18N
+        cmdNuevo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdNuevoActionPerformed(evt);
+            }
+        });
+
+        cmdEditar.setText("Editar");
+        cmdEditar.setName("cmdEditar"); // NOI18N
+        cmdEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdEditarActionPerformed(evt);
+            }
+        });
+
+        cmdEliminar.setText("Eliminar");
+        cmdEliminar.setName("cmdEliminar"); // NOI18N
+        cmdEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdEliminarActionPerformed(evt);
+            }
+        });
+
+        cmdGuardar.setText("Guardar");
+        cmdGuardar.setEnabled(false);
+        cmdGuardar.setName("cmdGuardar"); // NOI18N
+        cmdGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdGuardarActionPerformed(evt);
+            }
+        });
+
+        cmdCancelar.setText("Cancelar");
+        cmdCancelar.setEnabled(false);
+        cmdCancelar.setName("cmdCancelar"); // NOI18N
+        cmdCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdCancelarActionPerformed(evt);
+            }
+        });
+
+        lblTotalRegs.setText("Total de registros: 0");
+
+        txtRUC.setEnabled(false);
+        txtRUC.setName("txfRUC"); // NOI18N
+
+        txtRazonSocial.setEnabled(false);
+        txtRazonSocial.setName("txfRazon"); // NOI18N
+
+        txtTelefono.setEnabled(false);
+        txtTelefono.setName("txfTelefono"); // NOI18N
+
+        txtDireccion.setEnabled(false);
+        txtDireccion.setName("txfDireccion"); // NOI18N
+
+        lblRuc.setText("RUC:");
+
+        lblRazonSocial.setText("Razón social:");
+
+        lblDireccion.setText("Dirección:");
+
+        lblTelefono.setText("Teléfono:");
+
+        gridProveedores.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        gridProveedores.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                gridProveedoresMouseClicked(evt);
+            }
+        });
+        jScrollPane2.setViewportView(gridProveedores);
+
+        lblNombreComercial.setText("Nombre Comercial:");
+
+        txtNombreComercial.setEnabled(false);
+        txtNombreComercial.setName("txfTelefono"); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(cmdNuevo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmdGuardar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmdCancelar)
+                        .addGap(70, 70, 70)
+                        .addComponent(cmdEditar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmdEliminar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblRuc)
+                            .addComponent(lblRazonSocial)
+                            .addComponent(lblDireccion)
+                            .addComponent(lblTelefono)
+                            .addComponent(lblNombreComercial))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtRazonSocial, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtRUC, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtTelefono, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtDireccion, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtNombreComercial, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblTotalRegs)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 442, Short.MAX_VALUE))))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(13, 13, 13)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmdNuevo)
+                    .addComponent(cmdGuardar)
+                    .addComponent(cmdCancelar)
+                    .addComponent(cmdEditar)
+                    .addComponent(cmdEliminar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtRUC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblRuc))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtRazonSocial, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblRazonSocial))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblNombreComercial)
+                            .addComponent(txtNombreComercial, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(7, 7, 7)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblTelefono)
+                            .addComponent(txtTelefono, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 112, Short.MAX_VALUE))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblTotalRegs)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void cmdNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdNuevoActionPerformed
+        limpiar();
+        //accion = "nuevo";
+        habilitarModificacion(true);
+    }//GEN-LAST:event_cmdNuevoActionPerformed
+
+    private void cmdGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdGuardarActionPerformed
+     
+    }//GEN-LAST:event_cmdGuardarActionPerformed
+
+    private void cmdCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdCancelarActionPerformed
+       
+    }//GEN-LAST:event_cmdCancelarActionPerformed
+
+    private void cmdEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdEditarActionPerformed
+      
+    
+    }//GEN-LAST:event_cmdEditarActionPerformed
+
+    private void cmdEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdEliminarActionPerformed
+       
+    }//GEN-LAST:event_cmdEliminarActionPerformed
+
+    private void gridProveedoresMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_gridProveedoresMouseClicked
+    
+    }//GEN-LAST:event_gridProveedoresMouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DialogProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DialogProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DialogProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DialogProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the dialog */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                DialogProveedor dialog = new DialogProveedor(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdCancelar;
+    private javax.swing.JButton cmdEditar;
+    private javax.swing.JButton cmdEliminar;
+    private javax.swing.JButton cmdGuardar;
+    private javax.swing.JButton cmdNuevo;
+    private javax.swing.JTable gridProveedores;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel lblDireccion;
+    private javax.swing.JLabel lblNombreComercial;
+    private javax.swing.JLabel lblRazonSocial;
+    private javax.swing.JLabel lblRuc;
+    private javax.swing.JLabel lblTelefono;
+    private javax.swing.JLabel lblTotalRegs;
+    private javax.swing.JTextField txtDireccion;
+    private javax.swing.JTextField txtNombreComercial;
+    private javax.swing.JTextField txtRUC;
+    private javax.swing.JTextField txtRazonSocial;
+    private javax.swing.JTextField txtTelefono;
+    // End of variables declaration//GEN-END:variables
+}
