621ed3538c99451d0184f9e1d4464852ab9e8358
CristhianMotoche <cristhian.motoche@gmail.com>
2015-11-29 22:53:06 -0500

    Añadir circle.yml y prueba de cifrador (para probar ejecucion de pruebas)

---
+++ src/test/java/com/gofact/soporte/CifradorTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gofact.soporte;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author camm
+ */
+public class CifradorTest {
+    @Test
+    public void testMd5() {
+        System.out.println("md5");
+        assertEquals("hola", "hola");
+    }
+    
+}
